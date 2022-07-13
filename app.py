c898ed1b68c598954068c76f216ad7addbc8fddc
Alao D. I <70070334+invest41@users.noreply.github.com>
2022-07-14 00:21:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import re
 import flask
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
-#from gevent.wsgi import WSGIServer
+from gevent.wsgi import WSGIServer
 
 # Define a flask app
 app = Flask(__name__)
@@ -35,8 +35,8 @@ def index():
     #this section is used by gunicorn to serve the app on Heroku
 if __name__ == '__main__':
 
-        app.run()
+        #app.run()
     #uncomment this section to serve the app locally with gevent at:  http://localhost:5000
     # Serve the app with gevent 
-        #http_server = WSGIServer(('', 5000), app)
-        #http_server.serve_forever()
+        http_server = WSGIServer(('', 5000), app)
+        http_server.serve_forever()
