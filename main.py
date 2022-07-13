78286ea4ff29350f1b47c1e5c657bbdf897f4f44
Alao D. I <70070334+invest41@users.noreply.github.com>
2022-07-13 23:32:39 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+from __future__ import division, print_function
+import sys
+import os
+import glob
+import re
+
+
+
+# Flask utils
+import flask
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+#from gevent.wsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+
+
+
+
+
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+
+
+    
+
+    #this section is used by gunicorn to serve the app on Heroku
+if __name__ == '__main__':
+
+        app.run()
+    #uncomment this section to serve the app locally with gevent at:  http://localhost:5000
+    # Serve the app with gevent 
+        #http_server = WSGIServer(('', 5000), app)
+        #http_server.serve_forever()
